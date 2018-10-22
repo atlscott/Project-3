396d6cfdedac30e7ea5bbe5051fcc558c923a0c9
atlscott <atlscott1@gmail.com>
2018-10-22 13:18:38 -0400

    Lots of updates being pushed at once, github was down

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -17,8 +17,13 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
+          <Link className="nav-link" to="/dashboard">
+            Dashboard
+          </Link>
+        </li>
+        <li className="nav-item">
           <a
-            href="#"
+            href=""
             onClick={this.onLogoutClick.bind(this)}
             className="nav-link"
           >
@@ -38,12 +43,12 @@ class Navbar extends Component {
     const guestLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
-          <Link className="nav-link" to="register">
+          <Link className="nav-link" to="/register">
             Sign Up
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="login">
+          <Link className="nav-link" to="/login">
             Login
           </Link>
         </li>
